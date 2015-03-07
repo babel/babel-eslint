a35161d3aac2b07e21e0b8f7f517ac92671a58bc
Benoît Zugmeyer <bzugmeyer@gmail.com>
2015-03-07 20:03:51 +0100

    Write non-regression tests for closed issue

--- test/non-regression.js
+++ test/non-regression.js
@@ -36,4 +36,94 @@ describe("verify", function () {
     );
   });
 
+  it("EOL validation (issue #2)", function () {
+    verifyAndAssertMessages(
+      "module.exports = \"something\";",
+      { "eol-last": 1, "semi": 1 },
+      [ "1:1 Newline required at end of file but not found. eol-last" ]
+    );
+  });
+
+  it("Readable error messages (issue #3)", function () {
+    verifyAndAssertMessages(
+      "{ , res }",
+      {},
+      [ "1:2 Unexpected token" ]
+    );
+  });
+
+  it("Unused vars in JSX (issue #5)", function () {
+    verifyAndAssertMessages(
+      "var App = require('./App');\n" +
+      "module.exports = <App />;",
+      { "no-unused-vars": 1 },
+      []
+    );
+  });
+
+  it("Modules support (issue #5)", function () {
+    verifyAndAssertMessages(
+      "import Foo from 'foo';\n" +
+      "export default Foo;",
+      { },
+      []
+    );
+  });
+
+  it("Rest parameters (issue #7)", function () {
+    verifyAndAssertMessages(
+      "function foo(...args) { return args; }",
+      { "no-undef": 1 },
+      []
+    );
+  });
+
+  it("super keyword in class (issue #10)", function () {
+    verifyAndAssertMessages(
+      "class Foo { constructor() { super() } }",
+      { "no-undef": 1 },
+      []
+    );
+  });
+
+  it("Rest parameter in destructuring assignment (issue #11)", function () {
+    verifyAndAssertMessages(
+      "const [a, ...rest] = ['1', '2', '3']; module.exports = rest;",
+      { "no-undef": 1 },
+      []
+    );
+  });
+
+  it("JSX attribute names marked as variables (issue #12)", function () {
+    verifyAndAssertMessages(
+      "module.exports = <div className=\"foo\" />",
+      { "no-undef": 1 },
+      []
+    );
+  });
+
+  it("Multiple destructured assignment with compound properties (issue #16)", function () {
+    verifyAndAssertMessages(
+      "module.exports = { ...a.a, ...a.b };",
+      { "no-dupe-keys": 1 },
+      []
+    );
+  });
+
+  it("Arrow function with non-block bodies (issue #20)", function () {
+    verifyAndAssertMessages(
+      "\"use strict\"; () => 1",
+      { "strict": 1 },
+      []
+    );
+  });
+
+  it("await keyword (issue #22)", function () {
+    verifyAndAssertMessages(
+      "async function foo() { await bar(); }",
+      { "no-unused-expressions": 1 },
+      []
+    );
+  });
+
 });
