fd4ff6c0cee1f1b021322b9d6d518fcf1d74bb29
Henry Zhu <hi@henryzoo.com>
2016-09-27 11:38:55 -0400

    remove old code

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 var babylonToEspree = require("./babylon-to-espree");
-var pick           = require("lodash.pickby");
-var Module         = require("module");
-var path           = require("path");
-var parse          = require("babylon").parse;
-var t              = require("babel-types");
-var tt             = require("babylon").tokTypes;
-var traverse       = require("babel-traverse").default;
+var pick            = require("lodash.pickby");
+var Module          = require("module");
+var path            = require("path");
+var parse           = require("babylon").parse;
+var t               = require("babel-types");
+var tt              = require("babylon").tokTypes;
+var traverse        = require("babel-traverse").default;
 
 var estraverse;
 var hasPatched = false;
@@ -55,21 +55,10 @@ function monkeypatch() {
   estraverses.push(estraverse);
   Object.assign(estraverse.VisitorKeys, t.VISITOR_KEYS);
 
-  // monkeypatch estraverse-fb (only for eslint < 2.3.0)
-  try {
-    var estraverseFb = eslintMod.require("estraverse-fb");
-    estraverses.push(estraverseFb);
-    Object.assign(estraverseFb.VisitorKeys, t.VISITOR_KEYS);
-  } catch (err) {
-      // Ignore: ESLint v2.3.0 does not have estraverse-fb
-  }
-
   // ESLint v1.9.0 uses estraverse directly to work around https://github.com/npm/npm/issues/9663
   var estraverseOfEslint = eslintMod.require("estraverse");
-  if (estraverseOfEslint !== estraverseFb) {
-    estraverses.push(estraverseOfEslint);
-    Object.assign(estraverseOfEslint.VisitorKeys, t.VISITOR_KEYS);
-  }
+  estraverses.push(estraverseOfEslint);
+  Object.assign(estraverseOfEslint.VisitorKeys, t.VISITOR_KEYS);
 
   estraverses.forEach(function (estraverse) {
     estraverse.VisitorKeys.MethodDefinition.push("decorators");
