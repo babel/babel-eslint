f78855ba20f393d4bb398d64dcc5f259a387a40b
Teddy Katz <teddy.katz@gmail.com>
2017-12-24 22:09:59 -0500

    Avoid using the enhanced referencer after monkeypatching

--- test/babel-eslint.js
+++ test/babel-eslint.js
@@ -540,15 +540,30 @@ describe("Public API", () => {
     );
   });
 
-  it("still provides a custom scope with parseForESLint after calling parse()", () => {
+  /*
+   * This test ensures that the enhanced referencer does not get used if eslint-scope has already been
+   * monkeypatched, because this causes some correctness issues. For example, if the enhanced referencer
+   * is used after the original referencer is monkeypatched, type annotation references are counted twice.
+   */
+  it("does not visit type annotations multiple times after monkeypatching and calling parseForESLint()", () => {
     assertImplementsAST(
       espree.parse("foo", { sourceType: "module" }),
       babelEslint.parse("foo", {})
     );
-    const parseForESLintResult = babelEslint.parseForESLint("foo", {
-      eslintVisitorKeys: true,
-      eslintScopeManager: true,
-    });
-    assert(parseForESLintResult.visitorKeys);
+    const parseResult = babelEslint.parseForESLint(
+      "type Foo = {}; function x(): Foo {}",
+      {
+        eslintVisitorKeys: true,
+        eslintScopeManager: true,
+      }
+    );
+    assert(parseResult.visitorKeys);
+    assert(parseResult.scopeManager);
+
+    const fooVariable = parseResult.scopeManager.getDeclaredVariables(
+      parseResult.ast.body[0]
+    )[0];
+
+    assert.strictEqual(fooVariable.references.length, 1);
   });
 });
