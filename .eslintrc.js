58342e35644bcef785628b313074d8410ddead16
Przemysław Zalewski <przemyslaw.zalewski@sandstream.pl>
2019-08-19 22:19:45 +0200

    Remove self reference by inclusion of eslint-config-babel - extend manually instead

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,23 @@
 "use strict";
 
+const config = require("eslint-config-babel");
+
 module.exports = {
   root: true,
-  extends: "babel",
-  plugins: ["prettier"],
-  rules: {
+  extends: config.extends,
+  plugins: config.plugins.concat(["prettier"]),
+  rules: Object.assign(config.rules, {
     "max-len": "off",
     strict: "error",
     "prettier/prettier": "error",
-  },
-  env: {
+  }),
+  env: Object.assign(config.env, {
     node: true,
-  },
-  parserOptions: {
+  }),
+  parserOptions: Object.assign(config.parserOptions, {
     sourceType: "script",
-  },
+  }),
+  globals: config.globals,
   overrides: [
     {
       files: ["test/**/*"],
